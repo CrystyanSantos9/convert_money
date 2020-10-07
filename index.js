8f90559e47ac7e781ae1cd06723e477a887408e8
Crystyan S. Santos <47446514+CrystyanSantos9@users.noreply.github.com>
2020-10-07 00:56:56 -0300

    iniciando_aplicacao

--- index.js
+++ index.js
@@ -1,30 +1,55 @@
 const express = require('express')
-
 const app = express()
 
 //lida com os paths se windows /xx/ se linux etc\xx\
 const path = require('path')
 
 //configurando view engine
-app.set('view engine','ejs')
+app.set('view engine', 'ejs')
+
+//importando nosso código
+const convert = require('./lib/convert')
 
 //Diretório da view
-app.set('views',path.join(__dirname,'views'))
+app.set('views', path.join(__dirname, 'views'))
 //arquivos estáticos publicos
-app.use(express.static(path.join(__dirname,'public')))
+app.use(express.static(path.join(__dirname, 'public')))
 
 
 
-app.listen(3000,err=>{
+app.listen(3000, err => {
     console.log(err)
 })
 
 //criando rotas com os methodos
-app.get('/',(req,res)=>{
-    res.send({Mensagem:'Bem vindo'})
+app.get('/', (req, res) => {
+    res.send({ Mensagem: 'Bem vindo' })
 })
 
 //criando rota com renderização
-app.get('/home',(req,res)=>{
- res.render('home',{name:'Crystyan'})   
+app.get('/home', (req, res) => {
+    res.render('home', { name: 'Crystyan' })
+})
+
+//criando a rota cotacao
+app.get('/cotacao', (req, res) => {
+    //usando destruction assigment para extrair valor 
+    const { cotacao, quantidade } = req.query
+
+    //se valor existir
+    if (cotacao && quantidade) {
+        const conversao = convert.convert(cotacao, quantidade)
+        res.render('cotacao', {
+            error:false,
+            cotacao: convert.toMoney(cotacao),
+            quantidade: convert.toMoney(quantidade),
+            conversao: convert.toMoney(conversao),
+        })
+    } else {
+        res.render('cotacao', {
+            error: 'Valores inválidos'
+        })
+    }
+
+
 })
